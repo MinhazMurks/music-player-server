c834298179f544bc48561b60f287ec54b2ae1aeb
MinhazMurks <minhazabdullah1@gmail.com>
2022-07-03 00:40:53 -0400

    refactor to application yml

--- src/main/kotlin/minhaz/musicplayerserver/api/FeedController.kt
+++ src/main/kotlin/minhaz/musicplayerserver/api/FeedController.kt
@@ -1,6 +1,27 @@
 package minhaz.musicplayerserver.api
 
+import minhaz.musicplayerserver.repository.ArtistRepository
+import minhaz.musicplayerserver.repository.FeatureRepository
+import minhaz.musicplayerserver.repository.MusicUserRepository
+import minhaz.musicplayerserver.repository.PlaylistRepository
+import minhaz.musicplayerserver.repository.SongRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class FeedController
+@RequestMapping("feed")
+class FeedController(
+    private val playlistRepository: PlaylistRepository,
+    private val songRepository: SongRepository,
+    private val featureRepository: FeatureRepository,
+    private val musicUserRepository: MusicUserRepository,
+    private val artistRepository: ArtistRepository
+) {
+    @GetMapping()
+    fun getFeed(): ResponseEntity<String> {
+
+        return ResponseEntity.ok().body(playlistRepository.findAll().toString())
+    }
+}
