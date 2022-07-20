e6666bde0eb93705f4ba65fe42dbdaf281a05ed1
MinhazMurks <minhazabdullah1@gmail.com>
2022-07-20 01:41:10 -0400

    update populate-db script

--- src/main/resources/db.populate/populate-db.sql
+++ src/main/resources/db.populate/populate-db.sql
@@ -23,19 +23,17 @@ values ('a283cac0-8951-491e-b55a-68e29f5b7e8c', '7d45f414-72bb-465f-ae72-0deef85
        ('53e7d574-ad04-49e5-9079-4de6c152e5ca', '7d45f414-72bb-465f-ae72-0deef85c11c4');
 
 insert into playlist (id, creator, name)
-values ('aafadcf1-a913-4b0b-923d-fafa8a157fe5', '786a5b4d-4c1a-402c-9328-821cab655405');
+values ('aafadcf1-a913-4b0b-923d-fafa8a157fe5', '786a5b4d-4c1a-402c-9328-821cab655405', 'best playlist ever');
 
 insert into playlist_song (id, playlist, song)
 values ('603c0051-9f76-4a99-8726-3df89847ecbd', 'aafadcf1-a913-4b0b-923d-fafa8a157fe5',
         '7d45f414-72bb-465f-ae72-0deef85c11c4'),
-       ('603c0051-9f76-4a99-8726-3df89847ecbd', 'aafadcf1-a913-4b0b-923d-fafa8a157fe5',
-        'a283cac0-8951-491e-b55a-68e29f5b7e8c');
+       ('9034628e-c458-414e-834b-2d531eddc34b', 'aafadcf1-a913-4b0b-923d-fafa8a157fe5',
+        'e4250c35-5653-41b0-9d31-93fa112b244e');
 
-insert into album (id, artist, name, art)
-values ('e8e396a5-f354-456a-a92b-4cd501590af2', 'd7266122-908a-4791-b17a-55f7f9dd0958', 'mental',
-        'https://music-server-storage.s3.amazonaws.com/Images/album-placeholder-full.webp'),
-       ('7aec798e-6652-4efb-a127-c98c94cde4f6', 'a283cac0-8951-491e-b55a-68e29f5b7e8c', 'rainbow',
-        'https://music-server-storage.s3.amazonaws.com/Images/album-placeholder-full.webp');
+insert into album (id, artist, name)
+values ('e8e396a5-f354-456a-a92b-4cd501590af2', 'd7266122-908a-4791-b17a-55f7f9dd0958', 'mental'),
+       ('7aec798e-6652-4efb-a127-c98c94cde4f6', 'a283cac0-8951-491e-b55a-68e29f5b7e8c', 'rainbow');
 
 insert into album_song (id, album, song)
 values ('9ac60599-30be-4c1d-a312-01e87394cc35', 'e8e396a5-f354-456a-a92b-4cd501590af2',
@@ -45,6 +43,6 @@ values ('9ac60599-30be-4c1d-a312-01e87394cc35', 'e8e396a5-f354-456a-a92b-4cd5015
        ('4abfea91-2ea6-4d5b-82ab-102ada1879a5', 'e8e396a5-f354-456a-a92b-4cd501590af2',
         'b26934f1-619a-4731-9765-21b3546c717f'),
        ('5463d8f8-37e3-4f21-ae9a-ee8c078506ce', '7aec798e-6652-4efb-a127-c98c94cde4f6',
-        'a283cac0-8951-491e-b55a-68e29f5b7e8c'),
+        'e4250c35-5653-41b0-9d31-93fa112b244e'),
        ('c4567946-4b75-412e-ba55-de140c0d14c8', '7aec798e-6652-4efb-a127-c98c94cde4f6',
-        'a283cac0-8951-491e-b55a-68e29f5b7e8c');
+        '414cb26d-3cbb-4fe8-9569-5e3618771d84');
